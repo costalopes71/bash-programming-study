94610078201f568b33c217bbfcfef1669a176925
Joao Lopes <developer+joao.lopes@elo7.com>
2020-12-26 22:08:27 -0300

    aula 7 - deixa o codigo da tarefa mais limpo

--- mod1/tarefa_aula7.sh
+++ mod1/tarefa_aula7.sh
@@ -1,9 +1,6 @@
 #! /usr/bin/env bash
 
-PARAM1=$1
-QTDPARAMS=$#
-
-if [ $QTDPARAMS -lt 1 ]; then
+if [ $# -lt 1 ]; then
   echo "ERRO: o comando deve conter ao menos 1 (um) parâmetro"
   exit 1
 fi
@@ -11,4 +8,4 @@ fi
 # -x file  : retorna true se o arquivo tem permissao de escrita
 [ -x $0 ] && echo "Tem permissao de escrita"
 
-[ $PARAM1 -gt 10 ] && echo "Script:" $0 " PID:" $$
+[ $1 -gt 10 ] && echo "Script:" $0 " PID:" $$
