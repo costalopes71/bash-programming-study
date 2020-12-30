37a2cf156fe5f6408a48401b7b79ec5d4ec2dce2
Joao Lopes <developer+joao.lopes@elo7.com>
2020-12-30 03:05:07 -0300

    chave flag

--- mod3/chaves_flag.sh
+++ mod3/chaves_flag.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
 
+# -------------------------------------------------------------
+# VARIAVEIS
+# -------------------------------------------------------------
 USUARIOS="$(cat /etc/passwd | cut -d : -f 1)"
 MENSAGEM_USO="
   $(basename $0) - [OPÇÕES] 
@@ -8,8 +11,9 @@ MENSAGEM_USO="
     -v - Versão
     -s - Ordenar a saída
 "
-
 VERSAO="v1.1"
+CHAVE_ORDENA=0
+CHAVE_MAIUSCULO=0
 
 # -------------------------------------------------------------
 # TESTES
@@ -25,10 +29,14 @@ VERSAO="v1.1"
 # [ "$1" = "-s" ] && echo "$USUARIOS" | sort && exit 0
 
 case "$1" in 
-  -h) echo "$MENSAGEM_USO" && exit 0       ;;
-  -v) echo "$VERSAO" && exit 0             ;;
-  -s) echo "$USUARIOS" | sort && exit 0    ;;
-   *) echo "$USUARIOS"                     ;;
+  -h) echo "$MENSAGEM_USO" && exit 0 ;;
+  -v) echo "$VERSAO" && exit 0       ;;
+  -s) CHAVE_ORDENA=1                 ;;
+  -m) CHAVE_MAIUSCULO=1              ;;
+   *) echo "$USUARIOS"               ;;
 esac
 
 # echo "$USUARIOS"
+
+[ $CHAVE_ORDENA -eq 1 ] && echo "$USUARIOS" | sort && exit 0
+[ $CHAVE_MAIUSCULO -eq 1 ] && echo "$USUARIOS" | tr [a-z] [A-Z]
