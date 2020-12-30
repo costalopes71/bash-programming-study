ee02eea9727126c471afca167ead2b11dca4fbe1
Joao Lopes <developer+joao.lopes@elo7.com>
2020-12-30 01:51:47 -0300

    chaves_flag primeira versao (ifs)

--- mod3/chaves_flag.sh
+++ mod3/chaves_flag.sh
@@ -4,11 +4,25 @@ USUARIOS="$(cat /etc/passwd | cut -d : -f 1)"
 MENSAGEM_USO="
   $0 - [OPÇÕES] 
 
-  -h - Menu de ajuda
-  -v - Versão
-  -s - Ordenar a saída
+    -h - Menu de ajuda
+    -v - Versão
+    -s - Ordenar a saída
 "
 
 VERSAO="v1.0"
+
+# -------------------------------------------------------------
+# TESTES
+# -------------------------------------------------------------
+
+
+# -------------------------------------------------------------
+# EXECUCAO
+# -------------------------------------------------------------
+[ "$1" = "-h" ] && echo "$MENSAGEM_USO" && exit 0
+
+[ "$1" = "-v" ] && echo "$VERSAO" && exit 0
+
+[ "$1" = "-s" ] && echo "$USUARIOS" | sort && exit 0
 
 echo "$USUARIOS"
